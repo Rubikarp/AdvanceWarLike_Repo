9b5794566a0329d77cd1028f7bd446c012f25156
Arthur D <arthurdeleye@gmail.com>
2021-04-21 17:06:18 +0200

    Grid system and pointer

---
+++ Unity3DEmptyURP/Assets/Scripts/Scripts/Tile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public enum TileType
+{
+    Ground,
+    Water
+}
+
+[System.Serializable]
+public struct Tile
+{
+    public Vector2Int pos;
+    public TileType type;
+
+    //Constructor
+    public Tile(Vector2Int pos)
+    {
+        this.pos = pos;
+        this.type = TileType.Ground;
+    }
+    public Tile(Vector2Int pos, TileType type)
+    {
+        this.pos = pos;
+        this.type = type;
+    }
+}
